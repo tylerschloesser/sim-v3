b175f3dc6314d3e26a4214fc485093cdd48390a6
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-02 19:49:10 -0800

    refactor to link

--- packages/app/src/page/root.component.tsx
+++ packages/app/src/page/root.component.tsx
@@ -1,10 +1,22 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './root.module.scss'
 
 export function RootPage() {
+  const navigate = useNavigate()
+
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Welcome!</h1>
-      <button className={styles.button}>Test</button>
+      <a
+        href="world"
+        className={styles.link}
+        onClick={(e) => {
+          e.preventDefault()
+          navigate('world')
+        }}
+      >
+        World
+      </a>
     </div>
   )
 }
