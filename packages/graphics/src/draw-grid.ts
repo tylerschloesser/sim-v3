abb74cb05ba39a38398b49cc5b56f3cea782398e
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-03 17:06:49 -0800

    add basic transforms

--- packages/graphics/src/draw-grid.ts
+++ packages/graphics/src/draw-grid.ts
@@ -1,7 +1,21 @@
-import { vec2 } from 'gl-matrix'
+import { mat4, vec2 } from 'gl-matrix'
+import { mat4Scale, mat4Translate } from './util.js'
+
+const transform: mat4 = mat4.create()
 
 export function drawGrid(
   gl: WebGL2RenderingContext,
   center: vec2,
   cellSize: number,
-): void {}
+): void {
+  mat4.identity(transform)
+
+  // flip the y axis so it matches canvas/domxy
+  mat4Scale(transform, 1, -1)
+
+  // TODO document this
+  mat4Translate(transform, -1)
+
+  // TODO document this
+  mat4Scale(transform, 2)
+}
