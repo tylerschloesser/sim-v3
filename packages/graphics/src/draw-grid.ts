de64bdd1799f69e3f39f28c8e9fe142023da4841
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-04 12:09:08 -0800

    partial grid working

--- packages/graphics/src/draw-grid.ts
+++ packages/graphics/src/draw-grid.ts
@@ -1,12 +1,13 @@
 import { Vec2, Viewport, mod } from '@sim-v3/core'
 import { mat4 } from 'gl-matrix'
+import invariant from 'tiny-invariant'
 import { Context } from './context.js'
 import { mat4Scale, mat4Translate } from './util.js'
 
 const transform: mat4 = mat4.create()
 
 export function drawGrid(
-  { gl, uniforms }: Context,
+  { gl, uniforms, attributes, buffers }: Context,
   center: Vec2,
   cellSize: number,
   viewport: Viewport,
@@ -40,4 +41,51 @@ export function drawGrid(
   )
 
   gl.uniformMatrix4fv(uniforms.transform, false, transform)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.vertex.buffer)
+  // prettier-ignore
+  gl.vertexAttribPointer(attributes.vertex, 2, gl.FLOAT, false, 0, 0)
+  gl.enableVertexAttribArray(attributes.vertex)
+
+  const { matrices } = buffers.matrix
+
+  const cols = Math.ceil(viewport.size.x / cellSize) + 1
+  const rows = Math.ceil(viewport.size.y / cellSize) + 1
+
+  invariant(cols + rows <= matrices.length)
+
+  let mi = 0
+  for (let col = 0; col < cols; col++) {
+    const matrix = matrices.at(mi++)
+    invariant(matrix)
+
+    mat4.identity(matrix)
+    mat4Translate(matrix, col * cellSize, 0)
+    mat4Scale(matrix, 2, rows * cellSize)
+  }
+  for (let row = 0; row < rows; row++) {
+    const matrix = matrices.at(mi++)
+    invariant(matrix)
+
+    mat4.identity(matrix)
+    mat4Translate(matrix, 0, row * cellSize)
+    mat4Scale(matrix, cols * cellSize, 2)
+  }
+
+  invariant(mi === cols + rows)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.matrix.buffer)
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, buffers.matrix.data)
+
+  for (let i = 0; i < 4; i++) {
+    const index = attributes.matrix + i
+    gl.enableVertexAttribArray(index)
+    const offset = i * 16
+    // prettier-ignore
+    gl.vertexAttribPointer(index, 4, gl.FLOAT, false, 4 * 16, offset)
+    gl.vertexAttribDivisor(index, 1)
+  }
+
+  // prettier-ignore
+  gl.drawArraysInstanced(gl.TRIANGLE_STRIP, 0, 4, cols + rows)
 }
