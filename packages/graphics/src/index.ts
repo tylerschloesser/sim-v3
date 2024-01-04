c8f57184e042157c45db5a2f51ea004439ad7cce
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-03 16:26:42 -0800

    refactor graphics

--- packages/graphics/src/index.ts
+++ packages/graphics/src/index.ts
@@ -1,21 +1,31 @@
 import { Vec2 } from '@sim-v3/core'
+import curry from 'lodash/fp/curry.js'
 import invariant from 'tiny-invariant'
+
+export interface Graphics {
+  clear(): void
+  drawGrid(center: Vec2, cellSize: number): void
+}
 
 export async function initGraphics(
   canvas: HTMLCanvasElement,
-) {
+): Promise<Graphics> {
   const gl = canvas.getContext('webgl2')
   invariant(gl)
 
   return {
-    clear() {
-      gl.clearColor(1, 0, 0, 1)
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    },
-    drawGrid(center: Vec2, cellSize: number) {},
+    clear: () => clear(gl),
+    drawGrid: curry(drawGrid)(gl),
   }
 }
 
-export type Graphics = Awaited<
-  ReturnType<typeof initGraphics>
->
+function clear(gl: WebGL2RenderingContext): void {
+  gl.clearColor(1, 0, 0, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+}
+
+function drawGrid(
+  gl: WebGL2RenderingContext,
+  center: Vec2,
+  cellSize: number,
+): void {}
