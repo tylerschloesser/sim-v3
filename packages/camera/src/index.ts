0f14c47ff426841c93c282a872e6bb43c44f1d14
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-04 13:21:01 -0800

    add placeholder pointer move

--- packages/camera/src/index.ts
+++ packages/camera/src/index.ts
@@ -1,7 +1,19 @@
 import { Camera, InitFn, Vec2 } from '@sim-v3/core'
 
-export const initCamera: InitFn<Camera> = async () => {
+export const initCamera: InitFn<Camera> = async ({
+  canvas,
+  signal,
+}) => {
   const position: Vec2 = { x: 0, y: 0 }
   const zoom: number = 0.5
+
+  canvas.addEventListener(
+    'pointermove',
+    (ev) => {
+      console.log('pointermove', ev.clientX, ev.clientY)
+    },
+    { signal },
+  )
+
   return { position, zoom }
 }
