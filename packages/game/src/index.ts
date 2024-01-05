9c2d13e71d404d416306692dab259237bfa744d9
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-05 12:37:52 -0800

    refactor game

--- packages/game/src/index.ts
+++ packages/game/src/index.ts
@@ -2,11 +2,15 @@ import { Camera } from '@sim-v3/core'
 import { Graphics } from '@sim-v3/graphics'
 import { World } from '@sim-v3/world'
 
+export interface Game {
+  render(): void
+}
+
 export function initGame(
   world: World,
   camera: Camera,
   graphics: Graphics,
-) {
+): Game {
   return {
     render() {
       graphics.clear()
@@ -14,5 +18,3 @@ export function initGame(
     },
   }
 }
-
-export type Game = ReturnType<typeof initGame>
