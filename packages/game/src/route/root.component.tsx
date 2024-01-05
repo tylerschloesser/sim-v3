1f800066b6dbec1ca9d96e70b66e59aa8c8eae1c
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-05 13:01:20 -0800

    add tools path

--- packages/game/src/route/root.component.tsx
+++ packages/game/src/route/root.component.tsx
@@ -1,3 +1,29 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import invariant from 'tiny-invariant'
+import styles from './root.module.scss'
+
 export function Root() {
-  return <>TODO!</>
+  const worldId = useWorldId()
+  const navigate = useNavigate()
+
+  return (
+    <div className={styles.container}>
+      <a
+        className={styles.link}
+        href={`${worldId}/tools`}
+        onClick={(ev) => {
+          ev.preventDefault()
+          navigate('tools')
+        }}
+      >
+        Tools
+      </a>
+    </div>
+  )
+}
+
+function useWorldId() {
+  const params = useParams<{ id: string }>()
+  invariant(params.id)
+  return params.id
 }
--- packages/game/src/routes.tsx
+++ packages/game/src/routes.tsx
@@ -1,9 +1,14 @@
 import { RouteObject } from 'react-router-dom'
 import { Root } from './route/root.component.js'
+import { Tools } from './route/tools.component.js'
 
 export const routes: RouteObject[] = [
   {
     index: true,
     Component: Root,
+  },
+  {
+    path: 'tools',
+    Component: Tools,
   },
 ]
