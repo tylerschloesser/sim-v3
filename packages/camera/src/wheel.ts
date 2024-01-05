485dfc611d2363b431072e0026f141b6863dbe2e
Tyler Schloesser <tylerschloesser@gmail.com>
2024-01-04 17:11:22 -0800

    disable

--- packages/camera/src/wheel.ts
+++ packages/camera/src/wheel.ts
@@ -1,6 +1,8 @@
 import { Camera } from '@sim-v3/core'
 
 export function handleWheel(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   camera: Camera,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ev: WheelEvent,
 ): void {}
